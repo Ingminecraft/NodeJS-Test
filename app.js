0aa9615032f2669fcff9b430c11b3bb05d62260d
Ingminecraft <chaumas16@gmail.com>
2021-12-21 17:58:02 +0700

    จบบทที่ 2

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const debug = require('debug','app');
 const port = 3000;
 const morgan = require('morgan');
+const part = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(part.join(__dirname,"/public/")))
 
 app.get("/", (req,res) =>{
 
